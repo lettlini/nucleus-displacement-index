bf49b0162a83a8b17eb8872e58291deef1d3721a
Kolya Lettl <klettl@posteo.com>
2025-01-21 16:20:23 +0100

    added `setup.py`

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="nucleus-displacement-index",
+    version="1.0",
+    author="Kolya Lettl",
+    author_email="kolya.lettl@uni-leipzig.de",
+    description="library for calculating the nucleus displacement index (NusDI)",
+    packages=find_packages(),
+    install_requires=["numpy", "opencv-python", "shapely"],
+)
